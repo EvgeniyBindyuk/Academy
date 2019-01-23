efe52d9cd3c573ccc290c81053de6b144c4e00a0
Evgeniy Bindyuk <evganius@gmail.com>
2019-01-24 00:29:22 +0300

    'start'

---
+++ src/components/info.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Info extends React.Component {
+    render() {
+        return (
+<div>
+    <h2>Пройти тест</h2>
+    <p>Найди себя в жизни!</p>
+</div>
+        );
+    }
+}
+
+export default Info;
